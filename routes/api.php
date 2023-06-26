e273238feed04cdc28ef66309fad704e2ad4799f
john ore <jore@g2o.com>
2023-06-26 10:36:54 -0400

    login/token geration route should go first

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ResourceController;
-use App\Http\Controller\UserController;
+use App\Http\Controllers\UserController; // Fixed typo here
+
+// Generate auth token for specific user
+Route::post('users/{id}/token', [UserController::class, 'generateToken'])->name('login');
 
 // Endpoints
 Route::middleware('auth:sanctum')->group(function () {
   Route::apiResource('resources', ResourceController::class);
   Route::apiResource('users', UserController::class);
 });
-
-// Generate auth token for specific user
-Route::post('users/{id}/token', [UserController::class, 'generateToken']);
